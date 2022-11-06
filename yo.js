fb19711be62a152bd16541d66673a441fe930efe
GLORIENZE <habeebsalehalhussain@gmail.com>
2022-11-06 22:31:16 +0530

    update

---
+++ yo.js
@@ -0,0 +1,13 @@
+//let a=window.prompt("what is your name?");
+//let b=window.prompt("what is your age?");
+//console.log("your name is",a,"and your age is",b)
+//document.write("hello ",a,",u r ",b," years old :)");
+//a=window.prompt("email");
+//b=window.prompt("passd");
+let email=document.getElementById(email);
+let pas=document.getElementById(pas);
+function valid()
+{
+    if(email=="habeeb"&&pas=="habeeb")
+    return true;
+}
